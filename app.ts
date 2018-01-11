96963ff9022e2a6af9a3baa10fbd43b0197aecd5
jacsebastien <jac.sebastien@gmail.com>
2018-01-11 16:08:07 +0100

    enum and tuple types

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
 console.log("Hello World !");
+
+// tuple type
+let address: [string, number] = ["Mystreet", 99];
+// let address: [string, number] = ["Mystreet"]; // Nope
+
+// enum
+enum Color {
+    Gray,   // 0
+    Green = 100,  // 100
+    Blue,    // 101
+    Red = 2,    // 2
+    Purple  // 3
+};
+let myColor: Color = Color.Green;
+console.log(myColor); // 1
